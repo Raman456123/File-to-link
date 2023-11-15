1c86a5e6ea85b5d897ba4af348f6fdab88c64829
Raman456123 <103272817+Raman456123@users.noreply.github.com>
2023-11-15 20:35:08 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,7 +11,7 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '15812424'))
     API_HASH = str(getenv('API_HASH', '274bdc7e5061b8bc4aa42c1487cc19f2'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6244562554:AAHHtwiwYcUQ7EahMjWDc6oEX3OnKk_FVY4'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -19,7 +19,7 @@ class Var(object):
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1241165775 5149486631").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'pravin_boopathi'))
@@ -36,7 +36,7 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://shelby:Kumar396@cluster0.k3o9hwv.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'https://t.me/cinemadudes1'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'cinemadudes1'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))     
     SHORTLINK_URL = getenv('SHORTLINK_URL', 'tnshort.net')
     SHORTLINK_API = getenv('SHORTLINK_API', 'd0d892782ce7d93c095b28f636a54bb68e6407da')
